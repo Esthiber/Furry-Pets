e481f6d75df6902948cd5d9f336857146519c1fc
DELL <DELL@DESKTOP-7VMN3MI>
2025-06-27 15:23:16 -0400

    modelo de proveedores

---
+++ PawfectMatch/Models/Proveedores.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PawfectMatch.Models
+{
+    public class Proveedores
+    {
+
+        [Key]
+        public int ProveedorId { get; set; }
+        [Required]
+        public string? Nombre { get; set; }
+        [Required]
+        public string? Direccion { get; set; }
+        [Phone]
+        public string? Telefono { get; set; }
+        [EmailAddress]
+        public string? Email { get; set; }
+
+    }
+}
